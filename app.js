e0702e73c33208a04a1c01b76ae788c8cbddd436
doyun kim <2copskdi@gmail.com>
2024-04-26 15:22:51 +0900

    first

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import morgan from 'morgan';
+import tweetsRouter from './Router/tweets.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.use('/tweets', tweetsRouter);
+app.use((req,res,next) =>{
+    res.sendStatus(404);
+});
+
+app.listen(8080);
