7f5b0a10fa9043c9451f483927d3e361f9ec6858
doyun kim <2copskdi@gmail.com>
2024-05-03 10:01:20 +0900

    보안설정개선

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import tweetsRouter from './Router/tweets.js';
-
+import { config } from "./config.js";
 const app = express();
 
 app.use(express.json());
@@ -12,4 +12,4 @@ app.use((req,res,next) =>{
     res.sendStatus(404);
 });
 
-app.listen(8080);
+app.listen(config.host.port);
